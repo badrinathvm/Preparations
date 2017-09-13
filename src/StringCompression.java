6d3dbc20a60035cb144e61f13d54e71379dfd338
badrinathvm@gmail.com <badrinathvm@gmail.com>
2017-09-12 18:15:08 -0700

    Added string compression programme

---
+++ src/StringCompression.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Badri on 9/12/17.
+ */
+public class StringCompression {
+
+    public static void main(String[] args) {
+        System.out.println(StringCompression.compress("a"));
+    }
+
+    public static String compress(String str){
+        String out = "";
+        int sum =1;
+        for ( int i =0 ; i<str.length()-1;i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                sum++;
+            } else {
+                out = out + str.charAt(i) + sum;
+                sum = 1;
+            }
+        }
+
+        out = out + str.charAt(str.length()-1) + sum;
+
+        return out;
+    }
+}
